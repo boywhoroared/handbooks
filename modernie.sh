051e6c66c66a803f8c39b86bfb7360893c6bfa52
Mario Parris <mparris@northernlight.com>
2014-03-26 17:34:51 -0400

    add note for extracting and using IE vms

--- modernie.sh
+++ modernie.sh
@@ -1,5 +1,5 @@
 # Download modern.ie VMs
-# Use unrar to extract
+# Use unrar to extract ()
 
 # IE6.WinXP
 curl -O -L "http://www.modern.ie/vmdownload?platform=linux&virtPlatform=virtualbox&browserOS=IE6-WinXP&parts=1&filename=VirtualBox/IE6_WinXP/Linux/IE6.WinXP.For.LinuxVirtualBox.sfx"
@@ -7,4 +7,11 @@ curl -O -L "http://www.modern.ie/vmdownload?platform=linux&virtPlatform=virtualb
 curl -O -L "http://www.modern.ie/vmdownload?platform=linux&virtPlatform=virtualbox&browserOS=IE8-WinXP&parts=2&filename=VirtualBox/IE8_WinXP/Linux/IE8.WinXP.For.LinuxVirtualBox.part{1.sfx,2.rar}"
 # IE10.Win7
 curl -O -L "http://www.modern.ie/vmdownload?platform=linux&virtPlatform=virtualbox&browserOS=IE10-Win7&parts=4&filename=VirtualBox/IE10_Win7/Linux/IE10.Win7.For.LinuxVirtualBox.part{1.sfx,2.rar,3.rar,4.rar}"
+
+# Extracting
+# unrar e -or <path-to-sfx>
+
+
+# The `.ova` files are actually `tar` containers. You can extract the 
+# virtual machine disk images from these for use with virt-manager/qemu/qcow
 
