87137edb7f4e498d1a04a100b7923df1da4164fe
Mario Parris <mparris@northernlight.com>
2014-03-26 17:22:42 -0400

    download modern.ie virtual machines for testing IE

---
+++ modernie.sh
@@ -0,0 +1,10 @@
+# Download modern.ie VMs
+# Use unrar to extract
+
+# IE6.WinXP
+curl -O -L "http://www.modern.ie/vmdownload?platform=linux&virtPlatform=virtualbox&browserOS=IE6-WinXP&parts=1&filename=VirtualBox/IE6_WinXP/Linux/IE6.WinXP.For.LinuxVirtualBox.sfx"
+# IE8.WinXP
+curl -O -L "http://www.modern.ie/vmdownload?platform=linux&virtPlatform=virtualbox&browserOS=IE8-WinXP&parts=2&filename=VirtualBox/IE8_WinXP/Linux/IE8.WinXP.For.LinuxVirtualBox.part{1.sfx,2.rar}"
+# IE10.Win7
+curl -O -L "http://www.modern.ie/vmdownload?platform=linux&virtPlatform=virtualbox&browserOS=IE10-Win7&parts=4&filename=VirtualBox/IE10_Win7/Linux/IE10.Win7.For.LinuxVirtualBox.part{1.sfx,2.rar,3.rar,4.rar}"
+
